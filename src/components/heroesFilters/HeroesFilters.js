7bdd464c9555926344291f305bd265bb3ed55b9c
Вадим <andreeshinvadim13@gmail.com>
2024-02-24 16:29:17 +0200

    Make my code shorter and easier to understand

--- src/components/heroesFilters/HeroesFilters.js
+++ src/components/heroesFilters/HeroesFilters.js
@@ -3,33 +3,20 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import classNames from "classnames";
 
-import {
-  filtersFetching,
-  filtersFetched,
-  filtersFetchingError,
-  activeFilterChanged,
-} from "../../actions";
+import { fetchFilters } from "../../actions";
+import { activeFilterChanged } from "./heroesSlice";
 import Spinner from "../spinner/Spinner";
-
-// Задача для этого компонента:
-// Фильтры должны формироваться на основании загруженных данных
-// Фильтры должны отображать только нужных героев при выборе
-// Активный фильтр имеет класс active
 
 const HeroesFilters = () => {
   const { filters, filtersLoadingStatus, activeFilter } = useSelector(
-    (state) => state
+    (state) => state.filters
   );
   const dispatch = useDispatch();
   const { request } = useHttp();
 
   // Запрос на сервер для получения фильтров и последовательной смены состояния
   useEffect(() => {
-    dispatch(filtersFetching());
-    request("http://localhost:3001/filters")
-      .then((data) => dispatch(filtersFetched(data)))
-      .catch(() => dispatch(filtersFetchingError()));
-
+    dispatch(fetchFilters(request));
     // eslint-disable-next-line
   }, []);
 
